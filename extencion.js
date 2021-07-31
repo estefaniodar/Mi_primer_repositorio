d0c343fe4ec5c62609876a5be52c9b3fbdd7f1e4
Estefani Odar <estefaniodar@hotmail.com>
2021-07-31 18:00:18 -0300

    agregue pagina extencion.js

---
+++ extencion.js
@@ -0,0 +1,11 @@
+
+var cadena1 ="Bienvenidos ";
+var cadena2 =" a GP audi car y electromecanica";
+var resultadoCadenas = cadena1 + cadena2;
+console.log(resultadoCadenas);
+alert(resultadoCadenas);
+
+var nombre = prompt("ingrese su nombre");
+var edad = prompt("ingrese su edad");
+console.log( nombre  + "tiene una edad " + edad);
+alert("usted " + nombre + " de " + edad + " años, se encuentra navegando nuestro sitio web!");
