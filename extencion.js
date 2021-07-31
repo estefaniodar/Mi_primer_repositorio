6c6f2c2933b4020caaee9081f200291587c108ae
Estefani Odar <estefaniodar@hotmail.com>
2021-07-31 19:36:19 -0300

    modifique pagina extencion.js

--- extencion.js
+++ extencion.js
@@ -4,8 +4,15 @@ var cadena2 =" a GP audi car y electromecanica";
 var resultadoCadenas = cadena1 + cadena2;
 console.log(resultadoCadenas);
 alert(resultadoCadenas);
-
+//AGREGANDO CONSICIONALES
 var nombre = prompt("ingrese su nombre");
-var edad = prompt("ingrese su edad");
+var edadMayor =18;
+var edad = parseInt (prompt("ingrese su edad"));
+if(edad>=edadMayor){
+    alert("podes comprar en la tienda oneline");
+}else{
+    alert("no podes comprar en la tienda oneline");
+}
 console.log( nombre  + "tiene una edad " + edad);
 alert("usted " + nombre + " de " + edad + " años, se encuentra navegando nuestro sitio web!");
+
